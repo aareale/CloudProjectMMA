b7460bb5e4cd5d40c1351c098b835973cddddb1e
Andrea Reale <a.reale@studenti.local>
2019-06-21 12:53:00 +0200

    ok

--- index.php
+++ index.php
@@ -1,22 +1,29 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
+    <link rel="stylesheet" href="style.css">
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Homepage</title>
+    
 </head>
-<body>
-    <form name="invio_file"
-        action="file.php"
-        method="POST"
-        enctype="multipart/form-data">
-        Utente: <input type="email" name="email" />
-        Password <input type="password" name="password"/>
-        <input type="submit" name="Invio" />
-        <a href="forgot.php">Hai dimenticato la password?</a>
-        <a href="register.php">Non sei ancora registrato? Clicca qui</a>
 
-</form>
+<body>
+    <div style="text-align:center">
+        <form name="invio_file" action="file.php" method="POST" enctype="multipart/form-data">
+            Utente: <input type="email" name="email" />
+            Password <input type="password" name="password" />
+            <input type="submit" name="Invio" />
+            <h1><a style="font-size:20px" href="forgot.php">Hai dimenticato la password?</a>
+                <h1>
+
+                    <h1> <a style="font-size:20px" href="register.php">Non sei ancora registrato? Clicca qui</a>
+                        <h1>
+    </div>
+
+    </form>
 </body>
+
 </html>
