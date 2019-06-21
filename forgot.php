3a19cf4e1212a4dd070b0ee9621ee67f2650982c
Andrea Reale <a.reale@studenti.local>
2019-06-21 12:23:52 +0200

    ok

---
+++ forgot.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>forgot password</title>
+</head>
+<body>
+        <form name="invio_file"
+            action="*inviata*"
+            method="POST"
+            enctype="multipart/form-data">
+            email: <input type="email" name="email" />
+            <input type="submit" name="Invio" />
+        </form>
+    
+</body>
+</html>
---
+++ register.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Register</title>
+</head>
+<body>
+        <form name="invio_file"
+            action="LINK CONVALIDA EMAIL"
+            method="POST"
+            enctype="multipart/form-data">
+            Utente: <input type="email" name="email" />
+            Password <input type="password" name="password"/>
+            <input type="submit" name="Registrati" />
+        </form>
+</body>
+</html>
